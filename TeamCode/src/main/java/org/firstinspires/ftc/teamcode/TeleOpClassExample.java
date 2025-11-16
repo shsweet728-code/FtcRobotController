ffd638ca6044f8f58b50e9bb2037c4de0eb304fb
shsweet728-code <shsweet728@gmail.com>
2025-11-15 16:24:17 -0800

    Improved Teleop
    Broke out Launcher and ArcadeDrive into separate classes.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpClassExample.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.ArcadeDrive;
+import org.firstinspires.ftc.teamcode.mechanisms.Launcher;
+
+@TeleOp
+public class TeleOpClassExample extends OpMode {
+    ArcadeDrive drive = new ArcadeDrive();
+    Launcher launcher = new Launcher();
+
+    @Override
+    public void init() {
+        drive.init(hardwareMap);
+        launcher.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        drive.drive(-gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+        if (gamepad1.y) {
+            launcher.startLauncher();
+        }
+        else if (gamepad1.b) {
+            launcher.stopLauncher();
+        }
+
+        launcher.setTargetVelocity(gamepad1.right_trigger);
+        launcher.setTargetVelocity(-gamepad1.left_trigger);
+        /*
+        if (gamepad1.right_trigger > 0.1) {
+            launcher.setTargetVelocity(gamepad1.right_trigger);
+        }
+        else if (gamepad1.left_trigger > 0.1) {
+            launcher.setTargetVelocity(-gamepad1.left_trigger);
+        }
+         */
+
+        launcher.updateState();
+
+
+        telemetry.addData("State", launcher.getState());
+        telemetry.addData("Launcher Velocity", launcher.getVelocity());
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Launcher.java
@@ -0,0 +1,101 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.StarterBotTeleop;
+
+public class Launcher {
+    private final double FEED_TIME_SECONDS = 0.20;
+    private final double FULL_SPEED = 1.0;
+    private double LAUNCHER_TARGET_VELOCITY = 1125;
+    private final double STOP_SPEED = 0.0;
+    private double LAUNCHER_MIN_VELOCITY = 1075;
+    private DcMotorEx launcher;
+    private CRServo leftFeeder, rightFeeder;
+    ElapsedTime feederTimer = new ElapsedTime();
+    private enum LaunchState {
+        IDLE,
+        SPIN_UP,
+        LAUNCH,
+        LAUNCHING,
+    }
+    private LaunchState launchState;
+
+    public void init(HardwareMap hwMap) {
+        launcher = hwMap.get(DcMotorEx.class, "launcher");
+        leftFeeder = hwMap.get(CRServo.class, "leftFeeder");
+        rightFeeder = hwMap.get(CRServo.class, "rightFeeder");
+
+        launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launcher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(
+                300, 0, 0, 10
+        ));
+        leftFeeder.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        launchState = LaunchState.IDLE;
+        stopFeeder();
+        stopLauncher();
+    }
+
+    public void stopFeeder() {
+        leftFeeder.setPower(STOP_SPEED);
+        rightFeeder.setPower(STOP_SPEED);
+    }
+
+    public void updateState() {
+        switch (launchState) {
+            case IDLE:
+                break;
+            case SPIN_UP:
+                launcher.setVelocity(LAUNCHER_TARGET_VELOCITY);
+                if (launcher.getVelocity() >= LAUNCHER_MIN_VELOCITY) {
+                    launchState = LaunchState.LAUNCH;
+                }
+            case LAUNCH:
+                leftFeeder.setPower(FULL_SPEED);
+                rightFeeder.setPower(FULL_SPEED);
+                feederTimer.reset();
+                launchState = LaunchState.LAUNCHING;
+                break;
+            case LAUNCHING:
+                if (feederTimer.seconds() > FEED_TIME_SECONDS) {
+                    stopFeeder();
+                    launchState = LaunchState.IDLE;
+                }
+                break;
+        }
+    }
+
+    public void startLauncher() {
+        if (launchState == LaunchState.IDLE) {
+            launchState = LaunchState.SPIN_UP;
+        }
+    }
+
+    public void stopLauncher() {
+        stopFeeder();
+        launcher.setVelocity(STOP_SPEED);
+        launchState = LaunchState.IDLE;
+    }
+
+    public double getVelocity() {
+        return launcher.getVelocity();
+    }
+
+    public String getState() {
+        return launcher.toString();
+    }
+
+    public void setTargetVelocity(double velocity) {
+        LAUNCHER_TARGET_VELOCITY += velocity;
+        LAUNCHER_MIN_VELOCITY += velocity;
+    }
+}
